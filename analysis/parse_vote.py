e491e4a0f4dc9685070477bf16af1956e52dedd8
gzp <gzp@gzp.(none)>
2015-11-07 23:59:22 +0800

    modify the parse_vote.py

--- analysis/parse_vote.py
+++ analysis/parse_vote.py
@@ -1,42 +1,37 @@
 import re
 import json
 
-#f1 = open('a1.json', 'r')
-#f2 = open('qapair.txt', 'w')
 
-
-
+f = open('qapair.txt', 'w')
 #line = f1.readline()
 #print line 
 
 path = 'a1.json'
 a = open(path)
 b = a.read()
-#print b[0]
 records = json.loads(b)
-#print records[0]
+#vote = []
+print records[0]
+print len(records[0])
 #print type(records[0]['vote_up'][0])
 #print records[0]['vote_down'][0]
 
-for i in range(0,10):
-    vd = records[i]['vote_down']
-    vp = records[i]['vote_up']
-    print vp
-    print type(vp)
-    length = len(vp)
-    print length
-    for j in range(0,length):
-        print j
-        vote = int(vp[j]) - int(vd[j])
+for i in range(0,1972):
+    vote = []
+    qid = records[i]['related_id']
+    f.write(qid)
+    if len(records[i]) > 1:
+        vd = records[i]['vote_down']
+        vp = records[i]['vote_up']
+        #print vp
+        #print vd
+        length = len(vp)
+        for j in range(0,length):
+            v = int(vp[j]) - int(vd[j])
+            f.write(','+str(v))
+            vote.append(v) 
         print vote
-
-#    print vote[j]
-
-
-#records = [json.loads(line) for line in open(path)]
-
-#print records[0]
-
-
-#f1.close()
-#f2.close()
+    f.write('\n')
+    f.flush()
+    
+f.close()
